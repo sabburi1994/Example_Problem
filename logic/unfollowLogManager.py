a059b71dcf6370d40b6a6a5596e750bc4b21dc08
sai nikhil <37330741+sabburi1994@users.noreply.github.com>
2018-08-31 15:37:36 +0200

    Update unfollowLogManager.py

--- logic/unfollowLogManager.py
+++ logic/unfollowLogManager.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 from contextlib import contextmanager
 
 from sqlalchemy import create_engine
@@ -7,7 +8,7 @@ import os
 from datetime import datetime, timedelta
 
 #added by abburi
-from db import AlchemyManager.DbManagerAlchemy as dbm
+import sqlite3
 
 USERNAME = 'postgres'
 PASSWORD = 'pass123'
@@ -32,26 +33,51 @@ class UnfollowLogManager(object):
     def __init__(self, insta_data_id):
         self.insta_data_id = insta_data_id
 
-    def start(self):
-        self.getDataFromBotLog()
-        self.saveDataInDb()
+#added by abburi
+    def db_connection(self, db_name):
+        connection = sqllite3.connect(db_name)
+        cursor = connection.cursor()
+        return connection, cursor
 
-    def correctUnfollowLog(self, userArray):
-        for user in userArray:
-            data = UnfollowLogManager.start.getDataFromBotLog(user)
-            if data.followed == True:
-                dbm.insertData('follwed'=False)
+    def replace_data(self, connection, cursor, value):
+        query = "UPDATE inputbot_unfollowlog SET unfollowed = 0 WHERE following_name = ?"
+        cursor.execute(query, value)
+        connection.commit()
+
+    def insert_data(self, connection, cursor, value):
+        query = "INSERT INTO inputbot_unfollowlog (id, following_name, unfollowed, unfollowed_time, follow_time, insta_data_id) VALUES (?,?,?,0,?,?)"
+        cursor.execute(query, value)
+        connection.commit()
+
+    def get_data(self, connection, cursor, array):
+        query = "SELECT id, unfollowed from inputbot_unfollowlog"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        ids = []
+        following_name = []
+        for x in result:
+            ids.append(x[0])
+            following_name.append(x[1])
+        for i in range(len(array)):
+            if array[i] == following_name[i]:
+                    self.replace_data(connection, cursor, following_name[i])
             else:
-                pass
+                value = (array[i], following_name[i], 1, 0, datetime.now().date(), "USER%s"%i)
+                self.insert_data(connection, cursor, value)
+#end by abburi
+    def correctUnfollowLog(self, userArray):
+        print "Starting this function"
+        connecction, cursor = db_connection('log-data.db')
+        get_data(connection, cursor, UserArray)
 
     def getDataFromUnfollowLog(self, days=2, amountUserToUnfollow=0):
         try:
-
+            pass
         except Exception as e:
             print(e)
         return data_all
 
 if __name__ == "__main__":
     fl = UnfollowLogManager('niclasguenther')
-    users_list = fl.getDataFromDb(days=2, amountUserToUnfollow=20)
     fl.correctUnfollowLog(['insta1', 'insta2', 'insta4'])
+    users_list = fl.getDataFromUnfollowLog(days=2, amountUserToUnfollow=20)
